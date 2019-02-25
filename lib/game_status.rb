5326195028f96c86a598d81d668defdc2237bc1f
Brian Simonton <bsimonton78@gmail.com>
2019-02-25 19:57:04 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -46,3 +46,17 @@ def draw?(board)
     false
   end
 end
+
+
+def winner(board)
+  winner = nil
+  WIN_COMBINATIONS.each do |combo|
+    if combo.all? {|idx| board[idx] == "X"}
+      winner = "X"
+    elsif combo.all? {|idx| board[idx] == "O"}
+      winner = "O"
+    else
+    end
+  end
+  winner
+end
