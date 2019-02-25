b340a17267ff68cea3f9482182a70981a15f1ff1
Brian Simonton <bsimonton78@gmail.com>
2019-02-25 19:55:43 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,31 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,4,8], [2,4,6],[0,3,6], [1,4,7], [2,5,8]] 
+
+def won?(board)
+  
+  
+  
+end
+
+
+def full?(board)
+  
+  board.all? do |space|
+    space == "X" || space == "O"
+  end
+  
+end
+
+def draw?(board)
+  if full?(board)
+    if won?(board) == false
+      true
+    else 
+      false
+    end
+  else
+    false
+  end
+end
